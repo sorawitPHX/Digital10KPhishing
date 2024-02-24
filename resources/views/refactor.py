bfb8126e9e7ffa5477c6435892e0ab9d780ff767
sorawitPHX <114576982+sorawitPHX@users.noreply.github.com>
2024-02-25 01:25:56 +0700

    refactor สำหรับแก้ asset

--- resources/views/refactor.py
+++ resources/views/refactor.py
@@ -1,12 +1,19 @@
 import re
+import time
 
 file = open("index.blade.php", mode='r', encoding='utf-8')
+output = open(f'index_.blade.php', mode='w', encoding='utf-8')
 
 lines = file.readlines()
-pattern = r'("\.[\dA-Za-z./-_\u0E00-\u0E7F-]+")'
-replacement = r'{{ asset(\1) }}'
-for i in lines:
-    # print(i, end='')
-    check = re.search(string=i, pattern=pattern)
+pattern = r'("\.[\dA-Za-z./-_\u0E00-\u0E7F-()]+")'
+replacement = r"'{{ asset(\1) }}'"
+for i in range(0, lines.__len__()):
+    check = re.search(string=lines[i], pattern=pattern)
     if(check): 
-        print(re.sub(string=i, pattern=pattern, repl=replacement), end='')
+        a = re.sub(string=lines[i], pattern=pattern, repl=replacement)
+        a = a.replace('./', '')
+        lines[i] = a
+    output.write(lines[i])
+
+file.close()
+output.close()
