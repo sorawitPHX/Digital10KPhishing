9287c22afae35f116840e3591ad448f3114cd2cc
sorawitPHX <114576982+sorawitPHX@users.noreply.github.com>
2024-02-25 04:06:40 +0700

    Add bootstrap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,6 +15,10 @@
         crossorigin="anonymous" />
     {{-- link style --}}
     <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+    {{-- Bootstrap Style --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    {{-- Bootstrap Script --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <script id="GTMscript" async="" src=(1)123></script>
@@ -1125,7 +1129,7 @@
                                     _ngcontent-bii-c26="" id="btn-check-status" target=""
                                     class="btn arrow-right darkblue-gradient"
                                     href="{{ route('register') }}">
-                                    <div _ngcontent-bii-c26="">ลงทะเบียนเงินดิจิตอล</div>
+                                    <div _ngcontent-bii-c26="" class="text-center">ลงทะเบียนเงินดิจิตอล</div>
                                 </a></div>
                         </div>
                     </div>
