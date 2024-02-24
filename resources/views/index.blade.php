24122de6a17ee9dae79868dc4717c8e115c6cd87
sorawitPHX <114576982+sorawitPHX@users.noreply.github.com>
2024-02-25 01:48:59 +0700

    demo 1.0

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,19 @@
 <html lang="en">
 
 <head>
+    <!-- @fonts.google -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter&family=Kanit&family=Noto+Serif:wght@500&family=Playfair+Display:wght@400;600&family=Varela+Round&display=swap"
+        rel="stylesheet">
+    <!-- @font-awsome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" />
+    {{-- link style --}}
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <script id="GTMscript" async="" src=(1)123></script>
     <meta name="generator" content="Scully 1.0.0">
