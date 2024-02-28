94a59657051cb042a324b2708c1cea72f96200cc
PloyU <137448173+ArayaPloy@users.noreply.github.com>
2024-02-28 12:37:24 +0700

    แก้ผลลัพธ์ใหม่

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -29,32 +29,28 @@
         border: 1px solid rgb(195, 195, 195);
     }
 
-    .lock-icon {
+    .lock-icon{
         position: absolute;
         margin-top: 10px;
         right: 632;
     }
-
-    .lock-icon2 {
+    .lock-icon2{
         position: absolute;
         margin-top: 10px;
         right: 583;
     }
 </style>
 @section('success')
-<div class="container">
-    <div class="head mt-3 mb-5" style="text-align: center">
-        <h1>ลงทะเบียนสำเร็จ</h1>
-        <div class="col-6" style="display: flex; flex-direction: column; align-items: center;">
+    <div class="container">
+        <div class="head mt-3 mb-5" style="text-align: center">
+            <h1>ลงทะเบียนเสร็จสิ้น</h1>
+            <div class="col-6" style="display: flex; flex-direction: column; align-items: center;">
             <i class="bi bi-check-circle-fill" style="color: green; font-size: 64px;"></i>
+        
+        <div class="button mb-5" style="display: flex; justify-content: center;margin-top: 60px;" >
+            <button type="submit" style="margin-right: 10px; padding: 10px 60px; background-color: #00a6e6; color: #fff; border: none; border-radius: 30px; cursor: pointer;">กลับสู่หน้าหลัก</button>
+            <button type="button" style="padding: 10px 60px; background-color:white; color: gray; border: 1px solid rgb(195, 195, 195); border-radius: 30px; cursor: pointer;">ยกเลิก</button>
         </div>
     </div>
-
-    <div class="button mb-5" style="display: flex; justify-content: center; margin-top: 60px;">
-        <button type="submit" style="margin-right: 10px; padding: 10px 60px; background-color: #00a6e6; color: #fff; border: none; border-radius: 30px; cursor: pointer;">กลับสู่หน้าหลัก</button>
-        <button type="button" style="padding: 10px 60px; background-color:white; color: gray; border: 1px solid rgb(195, 195, 195); border-radius: 30px; cursor: pointer;">ยกเลิก</button>
-    </div>
-</div>
-
 
 @endsection
